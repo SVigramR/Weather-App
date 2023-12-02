90eb1eebc9447ac972a6c5136d9c8a28f9cc39ad
SVigramR <sabareeshvigram654@gmail.com>
2023-12-03 01:03:47 +0530

    Renamed the function to more suitable one

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
 const securityKey = "37431f2d56f34c9eb9c01543232711"
 
-async function getCurrentWeather(json, city) {
+async function getWeather(json, city) {
     const apiUrl = `https://api.weatherapi.com/v1/${json}.json?key=${securityKey}&q=${city}`
     const fetchCurrent = await fetch(apiUrl)
     const current = await fetchCurrent.json()
     return current
 }
 
-async function currentWeather() {
-    const current = await getCurrentWeather('current','adsf5666')
+async function modifyWeather() {
+    const current = await getWeather('current','adsf5666')
     console.log(current)
     if (current.error) console.log(current.error.message);
 }
 
-currentWeather()
+modifyWeather()
